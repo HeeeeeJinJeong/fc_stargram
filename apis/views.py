a1455cd3b9112f6ef89a248b309e2eebb2061178
HeeeeeJinJeong <misakiyoshikuni@gmail.com>
2019-08-07 21:26:25 +0900

    home.html repactoring

--- apis/views.py
+++ apis/views.py
@@ -25,25 +25,11 @@ class BaseView(View):
 
 class UserCreateView(BaseView):
     def post(self, request):
-        # Restful 하게 하기 위해선 POST, DELETE 다 나누어야 하지만, 편의상 POST만 사용하도록 하겠다.
-        username = request.POST.get('username', '')
-        if not username:
-            return self.response(message='아이디를 입력해주세요.', status=400)
-        password = request.POST.get('password', '')
-        if not password:
-            return self.response(message='패스워드를 입력해주세요.', status=400)
-        email = request.POST.get('email', '')
-        try:
-            validate_email(email)
-        except ValidationError:
-            return self.response(message='올바른 이메일을 입력해주세요.', status=400)
-
-        try:
-            user = User.objects.create_user(username, email, password)
-        except IntegrityError:
-            return self.response(message='이미 존재하는 아이디입니다.', status=400)
-
-        return self.response({'user.id': user.id})
+        text = request.POST.get('text', '').strip()
+        content = Content.objects.create(uesr=request.user, text=text)
+        for idx, file in enumerate(request.FILES.values()):
+            Image.objects.create(content=content, image=file, order=idx)
+        return self.response({})
 
 
 class UserLoginView(BaseView):
