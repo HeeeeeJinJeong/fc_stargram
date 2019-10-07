effc728ea498b994a832b3b9bd09842c14902b3b
HeeeeeJinJeong <misakiyoshikuni@gmail.com>
2019-10-07 15:39:17 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -20,23 +20,25 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from django.core.exceptions import ImproperlyConfigured
-import json
 
-secret_file = os.path.join(BASE_DIR, 'secret.json')
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+
+secret_file = os.path.join(BASE_DIR, 'secret.json') # secrets.json 파일 위치를 명시
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
 
 def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
     try:
-        print(secrets[setting])
         return secrets[setting]
     except KeyError:
-        error_msg = 'Set the {0} environment variable'.format(setting)
+        error_msg = "Set the {} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
 
-SECRET_KEY = get_secret('SECRET_KEY')
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
